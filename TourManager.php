c347822f630e3304b3ee339029f17cad29c6182c
pheroh <never_mind86@hotmail.com>
2016-09-28 10:27:50 +0300

    Update TourManager.php

--- TourManager.php
+++ TourManager.php
@@ -17,6 +17,7 @@ Class TourManager
         return count(self::$destinationCities);
     }
 
+    // As PHP doesn't have a native 0.0 to 1.0 random float, this method is added 
     static public function Random()
     {
         return mt_rand(0, mt_getrandmax() - 1) / mt_getrandmax();
